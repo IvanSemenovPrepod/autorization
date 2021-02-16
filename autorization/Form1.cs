60ecc82ca23202ad4e8d4242adfb892c9e348256
IvanSemenovTeacher <40857580+IvanSemenovTeacher@users.noreply.github.com>
2021-02-16 10:31:22 +0300

    Добавьте файлы проекта.

---
+++ autorization/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+using System.Security.Cryptography;
+
+namespace autorization
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string GetHash(string input)
+        {
+            var md5 = MD5.Create();
+            var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            return Convert.ToBase64String(hash);
+        }
+
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string login = textBox1.Text;
+            string password = textBox2.Text;
+            //првоеряем что логин и пароль введены
+            if (string.IsNullOrEmpty(login)|| string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("логин или пароль не введен");
+            }
+            else
+            {
+                //строка подключения из файла app.config
+                string con = ConfigurationManager.ConnectionStrings["DbDatabase"].ConnectionString;
+                //подключение к mysql
+                MySqlConnection connection = new MySqlConnection
+                {
+                    ConnectionString = con
+                };
+                //запрос sql для получения списка пользователей
+                //с введеными логином и паролем
+                //в нормальном случае должен быть только один
+                MySqlCommand command = new MySqlCommand
+                {
+                    Connection = connection
+                };
+                command.Parameters.AddWithValue("@login", login);
+                command.Parameters.AddWithValue("@password", GetHash(password));
+                command.CommandText = "SELECT COUNT(*) FROM user WHERE login=@login AND password=@password";
+                //подключаемся и выполняем запрос
+                //на случай, если сервер не работает, сделаем обработку исключения
+                try
+                {
+                    connection.Open();
+                    //в переменную countuser присвоится количество пользователей из базы данных
+                    int countUser = Convert.ToInt32(command.ExecuteScalar());
+                    //закрываем соединение
+                    connection.Close();
+                    //проверяем countUser
+                    if (countUser == 1)
+                    {
+                        //MessageBox.Show("Пользователь авторизован");
+                        Form2 frm = new Form2();
+                        this.Hide();
+                        if (frm.ShowDialog() != DialogResult.OK)
+                            this.Show();
+                        
+                    }
+                    else
+                    {
+                        MessageBox.Show("Пользователь не зарегистрирован");
+                    }
+                }
+                //exception ex лучше поменять на более точный exception
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ autorization/Form2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using Stimulsoft.Report;
+using Stimulsoft.Report.Dictionary;
+
+namespace autorization
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            //строка подключения из файла app.config
+            string con = ConfigurationManager.ConnectionStrings["DbDatabase"].ConnectionString;
+            //подключение к mysql
+            MySqlConnection connection = new MySqlConnection
+            {
+                ConnectionString = con
+            };
+            MySqlCommand command = new MySqlCommand
+            {
+                Connection = connection
+            };
+            command.CommandText = "SELECT * FROM tovar";
+            //подключаемся и выполняем запрос
+            //на случай, если сервер не работает, сделаем обработку исключения
+            try
+            {
+                connection.Open();
+                DataTable table = new DataTable();
+                table.Load(command.ExecuteReader());
+                dataGridView1.DataSource = table;
+                //закрываем соединение
+                connection.Close();
+            }
+            //exception ex лучше поменять на более точный exception
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var report = StiReport.CreateNewReport();
+
+            
+
+            report.Render();
+            report.Show();
+        }
+    }
+}
